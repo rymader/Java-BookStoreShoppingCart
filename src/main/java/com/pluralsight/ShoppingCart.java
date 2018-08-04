a9199ba112f439954b2ed4c5c369188fa02137c2
Ryan Mader <rymader@yahoo.com>
2018-08-03 22:20:11 -0500

    module2

--- src/main/java/com/pluralsight/ShoppingCart.java
+++ src/main/java/com/pluralsight/ShoppingCart.java
@@ -30,6 +30,14 @@ public class ShoppingCart {
   return cartItem;
  }
 
+ public void deleteCartItem(int index) {
+   try {
+    cartItems.remove(index);
+   } catch (IndexOutOfBoundsException e) {
+     e.printStackTrace();
+   }
+ }
+
  public ArrayList<CartItem> getCartItems() {
   return cartItems;
  }
